e57e8b7630e5add2f1b19195d0075047c8c7d9fb
Karthik Banjan <92287364+karthikbanjan@users.noreply.github.com>
2022-03-19 23:03:48 +0530

    Update Contains Duplicate.java

--- Java/217. Contains Duplicate/Contains Duplicate.java
+++ Java/217. Contains Duplicate/Contains Duplicate.java
@@ -3,14 +3,18 @@
 class Solution {
     
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        if (nums.length <= 1) {
+            return false;
+        }
         
-        for (int i = 1; i < nums.length; i++) {
-            if(nums[i] == nums[i - 1]) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+
+        for (int num : nums) {
+            if (!hs.add(num)) {
                 return true;
-            }
-        }    
-        
+            }    
+        }
+
         return false;
     }
 }
